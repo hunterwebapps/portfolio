8748b405957f42ffc7f981c76c4558b3630aa7be
Dwayne Hunter <hunter@hunterwebapps.com>
2022-07-12 15:52:58 -0400

    cleanup

--- js/components/blog-post.js
+++ js/components/blog-post.js
@@ -6,20 +6,20 @@ class BlogPost extends HTMLElement {
     if (index.value < 2) {
       this.classList.add('col-lg-6', 'col-md-6', 'col-12');
       this.innerHTML = `
-          <div class="news-thumb mb-4">
+          <div class="blog-thumb mb-4">
               <a href="${link.value}" target="_blank">
                   <img
                       src="${image.value}"
-                      class="img-fluid news-image"
+                      class="img-fluid blog-image"
                       alt="${title.value}"
                   >
               </a>
 
-              <div class="news-text-info news-text-info-large">
+              <div class="blog-text-info blog-text-info-large">
                   <span class="category-tag bg-danger">${category.value}</span>
 
-                  <h5 class="news-title mt-2">
-                      <a href="news-detail.html" class="news-title-link">
+                  <h5 class="mt-2">
+                      <a href="${link.value}" class="blog-title-link" target="_blank">
                           ${title.value}
                       </a>
                   </h5>
@@ -29,22 +29,22 @@ class BlogPost extends HTMLElement {
     } else {
       this.classList.add('col-lg-4', 'col-md-4', 'col-12');
       this.innerHTML = `
-          <div class="news-thumb mb-lg-0 mb-lg-4 mb-0">
-              <a href="${link.value}">
+          <div class="blog-thumb mb-lg-0 mb-lg-4 mb-0">
+              <a href="${link.value}" target="_blank">
                   <img
                       src="${image.value}"
-                      class="img-fluid news-image"
+                      class="img-fluid blog-image"
                       alt="${title.value}"
                   >
               </a>
 
-              <div class="news-text-info">
+              <div class="blog-text-info">
                   <span class="category-tag me-3 bg-info">${category.value}</span>
 
                   <strong>${moment(date.value).format('MMMM Do @ hh:mm a')}</strong>
 
-                  <h5 class="news-title mt-2">
-                      <a href="news-detail.html" class="news-title-link">
+                  <h5 class="blog-title mt-2">
+                      <a href="${link.value}" class="blog-title-link" target="_blank">
                           ${title.value}
                       </a>
                   </h5>
