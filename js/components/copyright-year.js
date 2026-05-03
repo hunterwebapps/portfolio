4df6749123cebb54dac78599fc12861a4d3f0125
HunterWebApps <hunter@hunterwebapps.com>
2026-05-02 23:50:05 -0400

    fix copyright year

--- js/components/copyright-year.js
+++ js/components/copyright-year.js
@@ -1,7 +1,7 @@
 class CopyrightYear extends HTMLElement {
   connectedCallback() {
     const year = new Date().getFullYear();
-    this.innerHTML = `<a href="https://applied3pl.com">\u00A9 ${year} Applied 3PL Systems</a>`;
+    this.textContent = year;
   }
 }
 
